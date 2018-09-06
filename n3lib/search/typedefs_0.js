44cf3a6e3ced8dd7e6c4114e3fbb4e553aad91cc
Davide Francesco Merico <hds619@gmail.com>
2018-09-06 22:14:01 +0200

    Update docs

--- n3lib/search/typedefs_0.js
+++ n3lib/search/typedefs_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['n3lact',['N3LAct',['../n3__header_8h.html#afb10e6f7012513b51225a4d3add36cae',1,'n3_header.h']]],
-  ['n3lcsvdata',['N3LCSVData',['../n3__header_8h.html#a5c7192b27e221d7a9c8785cb3ddef926',1,'n3_header.h']]],
+  ['n3lact',['N3LAct',['../n3__header_8h.html#a462534238a9cb6fb7c772409affb270e',1,'n3_header.h']]],
+  ['n3lcsvdata',['N3LCSVData',['../n3__header_8h.html#aa25c4fe50066ced6c15051cdf1b17353',1,'n3_header.h']]],
   ['n3llayer',['N3LLayer',['../n3__header_8h.html#a9ee3a7104816bdb6222148cfe9ca8ad9',1,'n3_header.h']]],
   ['n3lneuron',['N3LNeuron',['../n3__header_8h.html#a621b1df037f351bd3542298933e5799a',1,'n3_header.h']]],
   ['n3lweight',['N3LWeight',['../n3__header_8h.html#ac37c67a24ec253f5cd205cbc981922ca',1,'n3_header.h']]],
-  ['n3lweightgenerator',['N3LWeightGenerator',['../n3__header_8h.html#ab3b7d8e984e9ad2e4d5e7cbe433ad572',1,'n3_header.h']]]
+  ['n3lweightgenerator',['N3LWeightGenerator',['../n3__header_8h.html#afd79111731c35648d0752ae744c6ccb0',1,'n3_header.h']]]
 ];
